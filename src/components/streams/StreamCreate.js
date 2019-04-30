0007ecaeade0a3ab8cdb5e494bbb47cbb85ba10f
jixuni <jixuni@gmail.com>
2019-04-30 18:41:47 -0400

    added redux thunk

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,10 +25,10 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
-
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
+  u;
   render() {
     return (
       <form
@@ -59,7 +61,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate: validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
