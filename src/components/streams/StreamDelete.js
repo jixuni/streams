026d0e040c7cd45e3deacee35691e127db18d610
jixuni <jixuni@gmail.com>
2019-05-01 21:41:26 -0400

    added login to the buttons

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,22 +1,59 @@
 import React from "react";
 import Modal from "../modal";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-  const actions = (
-    <div>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </div>
-  );
-  return (
-    <div>
-      StreamDelete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderAction() {
+    const { id } = this.props.match.params;
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return " Are you sure you want to delete this stream?";
+    }
+
+    return ` Are you sure you want to delete this stream with title: ${
+      this.props.stream.title
+    }`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderAction()}
+        onDimiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-export default StreamDelete;
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
